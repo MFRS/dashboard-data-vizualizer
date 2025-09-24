648bf91ae37f8823e07a4f63bef3782f6ba19c17
Валерій Зінченко <zinchenkomac@icloud.com>
2025-09-24 09:17:50 +0200

    fix-2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path-browserify";
-import tailwindcss from '@tailwindcss/vite'
-
-
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@shared": path.resolve(__dirname, "../../shared"),
     },
+  },
+  server: {
+    allowedHosts: [
+      "dashboard-data-vizualizer-fe.onrender.com", // ✅ your frontend domain
+    ],
+    host: "0.0.0.0", // ✅ recommended if deploying dev server
+    port: +(process?.env?.PORT || 5173),
   },
 });
